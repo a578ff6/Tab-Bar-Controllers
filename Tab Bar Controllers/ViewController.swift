6d1050212ea50952f79b5188a6be2f516b3332e7
wayne tsao <a578ff6@gmail.com>
2023-09-28 21:45:42 +0800

    Initial Commit

---
+++ Tab Bar Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab Bar Controllers
+//
+//  Created by 曹家瑋 on 2023/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
